64affbdf4121e802f8569ea9a4dcb1113bbfccb2
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-22 06:40:32 +0600

    Project Genesis

---
+++ lib/apkpure_scraper.ex
@@ -0,0 +1,18 @@
+defmodule ApkpureScraper do
+  @moduledoc """
+  Documentation for `ApkpureScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ApkpureScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/apkpure_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule ApkpureScraperTest do
+  use ExUnit.Case
+  doctest ApkpureScraper
+
+  test "greets the world" do
+    assert ApkpureScraper.hello() == :world
+  end
+end
