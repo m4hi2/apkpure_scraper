b1eb3caff1d94b9047b6470a5e79943ad417466f
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-23 23:15:35 +0600

    feat: custom user agent to pose as adbot

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,7 @@
 import Config
+
+config :crawly,
+  closespider_timeout: 10,
+  middlewares: [
+    {Crawly.Middlewares.UserAgent, user_agents: ["Mozilla/5.0 (Linux; Android 5.0; SM-G920A) AppleWebKit (KHTML, like Gecko) Chrome Mobile Safari (compatible; AdsBot-Google-Mobile; +http://www.google.com/mobile/adsbot.html)"]}
+  ]
