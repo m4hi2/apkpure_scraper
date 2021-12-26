68073e996cc32074f3e10fcfe7adc28e9a14d909
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-26 13:55:47 +0600

    feat: schema for app information

---
+++ lib/apkpure_scraper/apps/app.ex
@@ -0,0 +1,14 @@
+defmodule ApkpureScraper.Apps.App do
+  use Ecto.Schema
+
+  schema "apps" do
+    field :app_name, :string, null: false
+    field :author, :string, null: false
+    field :category, :string, null: false
+    field :version, :string, null: false
+    field :playstore_link, :string, null: false
+    field :app_description, :string, null: false
+    field :app_icon, :string, null: false
+    field :images, {:array, :string}, null: false
+  end
+end
