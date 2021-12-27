2f5abd6ba8ed0a1184b13936d9c773389ffab78c
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-27 12:51:02 +0600

    change: playstore allowed to be null for testing

--- lib/apkpure_scraper/apps/app.ex
+++ lib/apkpure_scraper/apps/app.ex
@@ -6,7 +6,7 @@ defmodule ApkpureScraper.Apps.App do
     field :author, :string, null: false
     field :category, :string, null: false
     field :version, :string, null: false
-    field :playstore_link, :string, null: false
+    field :playstore_link, :string
     field :app_description, :string, null: false
     field :app_icon, :string, null: false
     field :images, {:array, :string}, null: false
