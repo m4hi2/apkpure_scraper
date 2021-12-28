5e27734ab78e77e019336e53e8c9256887d52e43
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-28 11:42:38 +0600

    feat: changeset for app schema

--- lib/apkpure_scraper/apps/app.ex
+++ lib/apkpure_scraper/apps/app.ex
@@ -1,5 +1,6 @@
 defmodule ApkpureScraper.Apps.App do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "apps" do
     field :app_name, :string, null: false
@@ -10,5 +11,22 @@ defmodule ApkpureScraper.Apps.App do
     field :app_description, :string, null: false
     field :app_icon, :string, null: false
     field :images, {:array, :string}, null: false
+
+    timestamps()
+  end
+
+  def changeset(apps, attrs \\ %{}) do
+    apps
+    |> cast(attrs, [
+      :app_name,
+      :author,
+      :category,
+      :version,
+      :playstore_link,
+      :app_description,
+      :app_icon,
+      :images
+  ])
+
   end
 end
