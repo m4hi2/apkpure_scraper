0c26ab02985a4b4be5473337e3876524e88bdec1
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-12-29 12:07:40 +0600

    feat: filter based on store link availability

--- lib/apkpure_scraper/apps.ex
+++ lib/apkpure_scraper/apps.ex
@@ -4,4 +4,13 @@ defmodule ApkpureScraper.Apps do
   alias ApkpureScraper.Repo
   alias ApkpureScraper.Apps.App
 
+  def filter_only_with_play_link() do
+    App
+    |> app_playstore_link_query()
+    |> Repo.all()
+  end
+
+  defp app_playstore_link_query(query) do
+    from(a in query, where: not is_nil(a.playstore_link))
+  end
 end
